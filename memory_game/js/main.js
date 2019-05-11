66773170b59a36dff2fe5c214d830951c4de16e2
Austin Blaine <austinblaine@Austins-MacBook-Pro.local>
2019-05-11 17:57:20 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,18 @@
-console.log("Up and Running!");
-var cardOne = "queen";
-var cardTwo =  "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards=["queen", "queen", "king", "king"];
+var cardsInPlay=[];
 
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardThree);
+var cardOne=cards[2];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo=cards[3];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length===2) {
+    if (cardsInPlay[0]===cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again");
+    }
+}
