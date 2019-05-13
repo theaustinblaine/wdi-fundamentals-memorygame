6be70a03f74a349314698204d5b64b8bf785fe5f
Austin Blaine <austinblaine@Austins-MacBook-Pro.local>
2019-05-13 17:51:48 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,39 @@
 var cards=["queen", "queen", "king", "king"];
 var cardsInPlay=[];
 
-var cardOne=cards[2];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
+function checkForMatch() {
+    if (cardsInPlay.length===2) {
+        if (cardsInPlay[0]===cardsInPlay[1]) {
+            console.log("You found a match!");
+        } else {
+            console.log("Sorry, try again");
+        }
+    }
+};
 
-var cardTwo=cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+function flipCard(cardId) {
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+};
 
-if (cardsInPlay.length===2) {
+flipCard(0);
+flipCard(2);
+
+
+/* code below is "deleted" but maybe supposed to stay?? confirm later.
+    var cardOne=cards[0];
+    cardsInPlay.push(cardOne);
+    console.log("User flipped " + cardOne);
+    
+    var cardTwo=cards[1];
+    cardsInPlay.push(cardTwo);
+    console.log("User flipped " + cardTwo);*/
+
+/*if (cardsInPlay.length===2) {
     if (cardsInPlay[0]===cardsInPlay[1]) {
         alert("You found a match!");
     } else {
         alert("Sorry, try again");
     }
-}
+}*/
