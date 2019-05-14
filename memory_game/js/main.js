c4f805581db7991e0eda3cd634dab4ea1e652eab
Austin Blaine <austinblaine@Austins-MacBook-Pro.local>
2019-05-14 13:47:37 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards=["queen", "queen", "king", "king"];
+var cards=[
+    {
+        rank: "queen",
+        suit: "Hearts",
+        cardImage: "images/queen-of-hearts.png",
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png",
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png",
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay=[];
 
 function checkForMatch() {
@@ -12,24 +33,17 @@ function checkForMatch() {
 };
 
 function flipCard(cardId) {
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
     checkForMatch();
 };
 
 flipCard(0);
 flipCard(2);
 
-
-/* code below is "deleted" but maybe supposed to stay?? confirm later.
-    var cardOne=cards[0];
-    cardsInPlay.push(cardOne);
-    console.log("User flipped " + cardOne);
-    
-    var cardTwo=cards[1];
-    cardsInPlay.push(cardTwo);
-    console.log("User flipped " + cardTwo);*/
-
+// this is an alert that is temporarily turned off
 /*if (cardsInPlay.length===2) {
     if (cardsInPlay[0]===cardsInPlay[1]) {
         alert("You found a match!");
